3ec3029f12f1537fa2d8cd3f4e20fb021cdc241f
Mahmoud Yasser <s-mahmoudyasser@zewailcity.edu.eg>
2022-04-20 14:24:26 +0200

    add  items to the club

--- db_handler.py
+++ db_handler.py
@@ -1,6 +1,8 @@
 # https://askubuntu.com/questions/1379425/system-has-not-been-booted-with-systemd-as-init-system-pid-1-cant-operate
 # sudo -b unshare --pid --fork --mount-proc /lib/systemd/systemd --system-unit=basic.target
 # sudo -E nsenter --all -t $(pgrep -xo systemd) runuser -P -l $USER -c "exec $SHELL"
+# sudo systemctl start mongod
+# sudo systemctl status mongod
 
 from pymongo import MongoClient
 from random import randint
@@ -35,6 +37,7 @@ def flask_resp(*, message_dict=None, resp_code, headers_opt=0):
 #Step 1: Connect to MongoDB - Note: Change connection string as needed
 client = MongoClient(port=27017)
 db=client.players_info
+db_store=client.store
 
 app = Flask(__name__)
 CORS(app, support_credentials=True)
@@ -53,8 +56,11 @@ def health_check():
 @app.route('/login', methods=['POST'])
 def login_api():
     contents = request.get_json(force=True)
+    print("contents:")
+    print(contents)
     login_result = login(contents["email"],contents["password"])
-    del login_result["data"]["_id"]
+    if login_result["available"]:
+        del login_result["data"]["_id"]
     print(login_result)
     return flask_resp(resp_code=200, message_dict=login_result)
 
@@ -98,6 +104,39 @@ def user_exists(email):
         return {'available': False, 'explain': 'No users found', 'data': None}
     except:
         return {'available': False, 'explain': "Couldn't retrieve from the database, check connections!", 'data': None}
+
+
+def item_exists(item_name):
+    try:
+        results = db_store.item_info.find({"item_name": item_name})
+        if results:
+            return  {'available': True, 'explain': None, 'data': results[0]}
+        return {'available': False, 'explain': 'No item found', 'data': None}
+    except:
+        return {'available': False, 'explain': "Couldn't retrieve from the database, check connections!", 'data': None}
+
+def add_item(item_name,description, cost):
+    is_already_available = item_exists(item_name)
+    if is_already_available['available']:
+        return {'created': False, 'error': 'user already available'}
+
+    try:
+        _ = db_store.item_info.insert_one({"item_name": item_name, "description": description,"cost": cost})
+        return {'created': True, 'error': None}
+    except Exception as exp:
+        return {'created': False, 'error': str(exp)}
+
+@app.route('/additem', methods=['POST'])
+def additem():
+    contents = request.get_json(force=True)
+    added_item = add_item(contents["item_name"],contents["description"],contents["cost"])
+    return flask_resp(resp_code=200, message_dict=signup_result)
+
+@app.route('/listitems', methods=['GET'])
+def list_all_items():
+    items_list = db_store.item_info.find({})
+    print(items_list)
+    return items_list
 
 if __name__ == "__main__":
     port = 5000
--- test_db.py
+++ test_db.py
@@ -1,6 +1,6 @@
-import request 
+import requests
 
-x= request.post("http://172.20.199.116:5000/login",json={"email":"yasser","password":"Ma"})
+x= requests.post("http://172.25.174.136:5000/login",json={"email":"yasser","password":"Ma"})
 
 
-x= requests.post("http://172.20.199.116:5000/login",json={"email":"yasser","password":"Ma"})
+x= requests.post("http://172.25.174.136:5000/login",json={"email":"yasser","password":"Ma"})
